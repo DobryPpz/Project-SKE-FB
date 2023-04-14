6c8f9ff1fd0ba3a83a7e34cc6b73be8d31fbf785
Aleksander <xaleksx960@gmail.com>
2023-04-14 10:41:28 +0200

    spring initializr begin

---
+++ demo/src/main/java/com/example/demo/ModzzakfransiemApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModzzakfransiemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModzzakfransiemApplication.class, args);
+	}
+
+}
