0b60d92c9b0e6399aedf7321910ddb422b2565af
ksi3m <mieszko9919@gmail.com>
2023-04-21 23:55:51 +0200

    First version of hangman rest api

---
+++ demo/src/main/java/com/example/demo/Hangman/other/TempClassForWords.java
@@ -0,0 +1,20 @@
+package com.example.demo.Hangman.other;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class TempClassForWords {
+
+    public static List<String> getWords() {
+        List<String> listOfWords = Arrays.asList
+                ("endomorphism","catalyst","sophia","spacesuit","spaghetti",
+                        "hemorrhoid","henceforth","cat", "dog", "pig","donkey","monkey",
+                        "black", "green", "blue","word","hangman","pattern", "book",
+                        "guitar","drums","cymbals","nothing","wrong","inside","job",
+                        "jet","fuel","steel","beams");
+        return listOfWords;
+    }
+
+
+}
