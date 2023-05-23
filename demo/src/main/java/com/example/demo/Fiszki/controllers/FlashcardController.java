7857b3672f52631afdf75aeef05b5343b781a8ac
Aleksander <xaleksx960@gmail.com>
2023-05-23 16:47:00 +0200

    post request to create flashcard set

--- demo/src/main/java/com/example/demo/Fiszki/controllers/FlashcardController.java
+++ demo/src/main/java/com/example/demo/Fiszki/controllers/FlashcardController.java
@@ -1,8 +1,11 @@
 package com.example.demo.Fiszki.controllers;
+import com.example.demo.Fiszki.models.FlashcardSet;
 import com.example.demo.Fiszki.service.FlashcardService;
 import com.example.demo.Fiszki.service.FlashcardSetService;
 import com.example.demo.Fiszki.service.FlashcardSetServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,4 +20,10 @@ public class FlashcardController {
         this.flashcardSetService = flashcardSetService;
     }
 
+    @PostMapping("/set")
+    public FlashcardSet addFlashcardSet(@RequestBody FlashcardSet flashcardSet){
+        flashcardSet.setId(0);
+        FlashcardSet dbSet = flashcardSetService.save(flashcardSet);
+        return dbSet;
+    }
 }
